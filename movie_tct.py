018d8f22f80d310e37ac434ce5f32a7a59607c96
fanofpspk <vamshichandra8587@gmail.com>
2025-04-03 14:09:38 +0530

    Add files via upload

---
+++ movie_tct.py
@@ -0,0 +1,15 @@
+tkt = input("Enter tkt name yes / no:")
+if (tkt == "yes"):
+    #MM
+    print("show me your tkt ")
+    tct_check = input(" show the tct")
+    if tct_check=="show":
+        print(f"well come to the sandhya70mm")
+        print("enjoy the film")
+        print("tnq for wtching the film")
+    else:
+        print("your tct is theft")
+else:
+    print("chul tkt teeskoni rapo ")
+print('the end')
+print("-"*50)
